02e3f1ff68e8fd6f7fad6281df65be2634df6624
Xiyang Chen <settinghead@gmail.com>
2016-08-29 01:16:24 -0400

    timer'

--- src/index.jsx
+++ src/index.jsx
@@ -2,20 +2,52 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 require('./roulette.styl');
 import React from 'react';
 import { render } from 'react-dom';
+import styles from './styles.css';
 
+var questions = {
+  1: "Foo",
+  2: "Bar",
+  3: "Baz3",
+  4: "Baz4",
+  5: "Baz5",
+  6: "Baz6",
+  7: "Baz7",
+  8: "Baz8",
+  9: "Baz9",
+};
 
 class App extends React.Component {
   constructor (props){
     super(props);
     this.spin = this.spin.bind(this);
+    this.state = {};
   }
   spin() {
-    this.setState({curr: 1 + Math.round(Math.random() * 9)});
+    this.setState({curr: 1 + Math.round(Math.random() * 9), spinning: true});
+    var _this = this;
+    this.timer = setTimeout(() => {
+      console.log('zzzz');
+      _this.setState({spinning: false});
+      _this.timer = null;
+    }, 1800);
+  }
+  componentWillUnmount () {
+    if(this.timer) {
+      clearTimeout(this.timer);
+      this.timer = null;
+    }
   }
   render() {
+    var title = '';
+    if (this.state.spinning) {
+      title = <h1>...</h1>;
+    } else {
+      title = <h1>{questions[this.state.curr]}</h1>;
+    }
+
     return (
-      <div>
-        <ul id="roulette" className={this.state && 'number-' + this.state.curr}>
+      <div className={styles.main}>
+        <ul id="roulette" className={'number-' + this.state.curr}>
           <li></li>
           <li></li>
           <li></li>
@@ -30,6 +62,7 @@ class App extends React.Component {
         <div className="controller">
           <button type="button" id="rollRnd" className="rnd btn btn-primary btn-lg" onClick={this.spin}>Spin</button>
         </div>
+        {title}
       </div>
     )
   }
